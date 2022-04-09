c344c72a6d857dd120c1683806358756fad2f2ca
André Gomes <andregs@users.noreply.github.com>
2022-04-09 19:31:27 +0200

    introduce ferris
    Signed-off-by: André Gomes <andregs@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use ferris_says::say;
+use std::{io::{stdout, BufWriter, Write}, time::Duration};
+
 fn main() {
-    println!("Hello, world!");
+    let stdout = stdout();
+    let mut writer = BufWriter::new(stdout.lock());
+    let mut count = 0;
+    let messages = vec![
+        "Hello, fellow Rustaceans!",
+        "I am Ferris.",
+        "Nice to meet you!",
+        "Ok, I'll say it again.",
+    ];
+
+    loop {        
+        let message = messages.get(count % messages.len()).unwrap();
+        let message = format!("{:04} - {}", count, message);
+        let width = message.chars().count();
+        
+        say(message.as_bytes(), width, &mut writer).unwrap();
+        println!("\n");
+
+        writer.flush().unwrap();
+        std::thread::sleep(Duration::from_secs(5));
+        count += 1;
+    }
 }
